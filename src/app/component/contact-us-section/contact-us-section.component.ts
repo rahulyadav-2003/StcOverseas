19b5a09919cf4ef24b9b6c0a2e62af279ed48f49
rahulyadav-2003 <rulydv2003@gmail.com>
2025-05-23 17:53:44 +0530

    add email id in contact form

--- src/app/component/contact-us-section/contact-us-section.component.ts
+++ src/app/component/contact-us-section/contact-us-section.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsSectionComponent implements OnInit {
 
+
+  mailOne: string = '';
+  mailTwo: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
